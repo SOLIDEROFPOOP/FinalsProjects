22768dfd41e18c1ba2b10e55a2418d866f36007a
Kapparov Murat <136073953+SOLIDEROFPOOP@users.noreply.github.com>
2023-12-13 06:05:56 +0600

    first save commit for the project

---
+++ 02-backend/spring-boot-library/src/main/java/com/muratkapparov/springbootlibrary/config/RestConfig.java
@@ -0,0 +1,31 @@
+package com.muratkapparov.springbootlibrary.config;
+
+import com.muratkapparov.springbootlibrary.entity.Book;
+import com.muratkapparov.springbootlibrary.entity.Review;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class RestConfig implements RepositoryRestConfigurer {
+    private String theAllowedOrigins = "http://localhost:3000";
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors){
+        HttpMethod[] unsupportedActions = {HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH, HttpMethod.PUT};
+        config.exposeIdsFor(Book.class, Review.class);
+        dissableHttpMethods(Book.class, config , unsupportedActions);
+        dissableHttpMethods(Review.class, config, unsupportedActions);
+        /* CORS config */
+        cors.addMapping(config.getBasePath() + "/**")
+                .allowedOrigins(theAllowedOrigins);
+    }
+    private void dissableHttpMethods(Class theClass , RepositoryRestConfiguration configuration, HttpMethod[] unsupportedActions){
+        configuration
+                .getExposureConfiguration()
+                .forDomainType(theClass)
+                .withAssociationExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedActions)));
+    }
+}
